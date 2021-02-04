c7519ab77b2a961a7ae6d6557232d761fd428d51
Jarutas Andritsch <jarutas.andritsch@solent.ac.uk>
2021-02-04 15:16:53 +0000

    Complete Q1

--- Week1/main.py
+++ Week1/main.py
@@ -1,1 +1,9 @@
-print ("Hello")
+def Q1():
+    print("Who broke my heart?")
+    response = input()
+    print(f"My first love, {response}, broke my heart for the first time.")
+    print("And I was like baby, baby, baby, oh.")
+
+
+#Call function
+Q1()
