509f0797c32c6fb7730a021f04f51e50314a524a
Jarutas Andritsch <jarutas.andritsch@solent.ac.uk>
2021-02-04 15:26:15 +0000

    Complete Q2

--- Week1/main.py
+++ Week1/main.py
@@ -4,6 +4,16 @@ def Q1():
     print(f"My first love, {response}, broke my heart for the first time.")
     print("And I was like baby, baby, baby, oh.")
 
+def Q2():
+    print("Who sees me rolling (Cops, Wardens, Other)?")
+    response = input()
+    if (response == "Cops"):
+        print ("They see me rolling. They hating. They hoping they gonna catch me riding dirty!")
+    elif (response == "Wardens"):
+        print("My music's so loud. I'm swanging. They hoping they gonna catch me riding dirty!")
+    else:
+        print("Trying to catch me riding dirty!")
 
 #Call function
 Q1()
+Q2()
