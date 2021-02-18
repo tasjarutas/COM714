9df5f68413230f8a7f6fea8b133821a7751c4119
Jarutas Andritsch <jarutas.andritsch@solent.ac.uk>
2021-02-18 14:43:24 +0000

    Create nonplanet class.

--- life/universe.py
+++ life/universe.py
@@ -1,9 +1,10 @@
 from planet import Planet
+from non_planet import NonPlanet
+import random
 
 
 class Universe:
-
-    id =0
+    id = 0
 
     def __init__(self):
         self.__planets = []
@@ -14,11 +15,15 @@ class Universe:
     def __str__(self):
         return f'The universe has (planets={len(self.__planets)}) planets.'
 
-    def generate(self) -> Planet:
-        Universe.id +=1
-        planet = Planet(f"Planet {Universe.id}")
-        self.__planets.append(planet)
-        return planet
+    def generate(self) -> None:
+        Universe.id += 1
+
+        if random.randint(0, 10) >=5:
+            planet = Planet(f"Planet {Universe.id}")
+            self.__planets.append(planet)
+        else:
+            non_planet = NonPlanet(f"NonPlanet {Universe.id}")
+            self.__planets.append(non_planet)
 
     def display(self) -> None:
         for planet in self.__planets:
